7f7db9b379df7c7d80009004740ebbb2d8117233
wonder-princess <sekai_love.me.do@hotmail.co.jp>
2021-09-29 07:51:20 +0000

    make search

--- app/javascript/src/hoge.js
+++ app/javascript/src/hoge.js
@@ -1,4 +1,5 @@
-console.log(1111222)
-$(document).on('turbolinks:load', function () {
-   $('.field').on('click.bs.dropdown.data-api', (event) => event.stopPropagation())
-})
+// console.log(1111222)
+
+// $(document).on('turbolinks:load', function () {
+//   $('.field').on('click.bs.dropdown.data-api', (event) => event.stopPropagation())
+// })
